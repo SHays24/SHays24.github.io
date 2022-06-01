a118ece40a54e61c983ad21b6b69f3ca1a83100f
SHays24 <81133539+SHays24@users.noreply.github.com>
2022-06-01 19:21:10 +1000

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(console.log('👍', 'appinstalled', event)); 
-self.addEventListener('fetch', (e) => {
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
-  );
-});
