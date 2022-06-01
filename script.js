7dd0b5570b2d9babf44ebf107c17b7967a00928a
SHays24 <81133539+SHays24@users.noreply.github.com>
2022-06-01 19:17:44 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,3 @@
-var x, y, target = null;
-document.addEventListener('mousedown', function(e) {
-  var clickedDragger = false;
-  for(var i = 0; e.path[i] !== document.body; i++) {
-    if (e.path[i].classList.contains("title-bar")) {
-      clickedDragger = true;
-    }
-    else if (clickedDragger && e.path[i].classList.contains('window')) {
-      target = e.path[i];
-      target.classList.add('dragging');
-      x = e.clientX - target.style.left.slice(0, -2);
-      y = e.clientY - target.style.top.slice(0, -2);
-      return;
-    }
-  }
-});
-document.addEventListener('mousemove', function(e) {
-  if (target === null) return;
-  target.style.left = e.clientX - x + 'px';
-  target.style.top = e.clientY - y + 'px';
-  var pRect = target.parentElement.getBoundingClientRect();
-  var tgtRect = target.getBoundingClientRect();
-  if (tgtRect.left < pRect.left) target.style.left = 0;
-  if (tgtRect.top < pRect.top) target.style.top = 0;
-  if (tgtRect.right > pRect.right) target.style.left = pRect.width - tgtRect.width + 'px';
-  if (tgtRect.bottom > pRect.bottom) target.style.top = pRect.height - tgtRect.height + 'px';
-});
-document.addEventListener('mouseup', function() {
-  if (target !== null) target.classList.remove('dragging');
-  target = null;
-  return;
-});
 window.fncShowHideList = function() {
     //alert('it ran: ');
     if (document.getElementById('tab1').getAttribute('class') === 'active') {
@@ -40,44 +8,20 @@ window.fncShowHideList = function() {
       document.getElementById('window').style.display = '';
   };
 };
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('sw.js')
-    .then(() => { console.log('Service Worker Registered'); });
-}
+const wrapper = document.querySelector(".window");
 
-// Code to handle install prompt on desktop
-/*
-let deferredPrompt;
-const addBtn = document.querySelector('add-button');
+const header = wrapper.querySelector(".title-bar");
 
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI to notify the user they can add to home screen
-
-
-  addBtn.addEventListener('click', (e) => {
-    // hide our user interface that shows our A2HS button
-
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-      });
-  });
+header.addEventListener("mousedown", function() {
+    document.onmousemove = function(e) {
+        var x = e.clientX;
+        var y = e.clientY;
+        wrapper.position = "relative";
+        wrapper.style.left = x + "px";
+        wrapper.style.top = y + "px";
+    };
 });
-window.addEventListener('appinstalled', (event) => {
-  console.log('👍', 'appinstalled', event);
-  // Clear the deferredPrompt so it can be garbage collected
-  window.deferredPrompt = null;
-});*/
+
+document.addEventListener("mouseup", function() {
+    document.onmousemove = null;
+});
