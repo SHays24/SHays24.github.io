e0e63e263312b1d8cbbeae672adf3d608533191b
Saxon Hays <81133539+SHays24@users.noreply.github.com>
2022-06-03 00:59:54 +0000

    added windows layering

--- script.js
+++ script.js
@@ -1,27 +1,64 @@
-window.fncShowHideList = function() {
+window.fncShowHideList = function(x) {
     //alert('it ran: ');
-    if (document.getElementById('tab1').getAttribute('class') === 'active') {
-      document.getElementById('tab1').setAttribute('class', 'minimized');     
-      document.getElementById('window').style.display = 'none';
+    if (document.getElementById('tab'+x).getAttribute('class') === 'active') {
+      document.getElementById('tab'+x).setAttribute('class', 'minimized');     
+      document.getElementById('window'+x).style.display = 'none';
     } else {
-        document.getElementById('tab1').setAttribute('class', 'active');
-      document.getElementById('window').style.display = '';
+        document.getElementById('tab'+x).setAttribute('class', 'active');
+      document.getElementById('window'+x).style.display = '';
   };
 };
-const wrapper = document.querySelector(".window");
-
-const header = wrapper.querySelector(".title-bar");
-
-header.addEventListener("mousedown", function() {
+const wrapper1 = document.querySelector("#window1");
+const wrapper2 = document.querySelector("#window2");
+const header1 = wrapper1.querySelector("#titlebar1");
+const header2 = wrapper2.querySelector("#titlebar2");
+header1.addEventListener("mousedown", function() {
     document.onmousemove = function(e) {
         var x = e.clientX;
         var y = e.clientY;
-        wrapper.position = "relative";
-        wrapper.style.left = x + "px";
-        wrapper.style.top = y + "px";
+        wrapper1.position = "relative";
+        wrapper1.style.left = x + "px";
+        wrapper1.style.top = y + "px";
     };
 });
-
+header1.addEventListener("touchstart", function() {
+    document.onmousemove = function(e) {
+        var x = e.clientX;
+        var y = e.clientY;
+        wrapper1.position = "relative";
+        wrapper1.style.left = x + "px";
+        wrapper1.style.top = y + "px";
+    };
+});
+header2.addEventListener("mousedown", function() {
+    document.onmousemove = function(e) {
+        var x = e.clientX;
+        var y = e.clientY;
+        wrapper2.position = "relative";
+        wrapper2.style.left = x + "px";
+        wrapper2.style.top = y + "px";
+    };
+});
+header2.addEventListener("touchstart", function() {
+    document.onmousemove = function(e) {
+        var x = e.clientX;
+        var y = e.clientY;
+        wrapper2.position = "relative";
+        wrapper2.style.left = x + "px";
+        wrapper2.style.top = y + "px";
+    };
+});
 document.addEventListener("mouseup", function() {
     document.onmousemove = null;
+});
+document.addEventListener("touchend", function() {
+    document.onmousemove = null;
+});
+window1.addEventListener("mousedown", function() {
+  document.getElementById("window1").setAttribute('class', 'focus window');
+  document.getElementById("window2").setAttribute('class', 'unfocus window');
+});
+window2.addEventListener("mousedown", function() {
+  document.getElementById("window2").setAttribute('class', 'focus window');
+  document.getElementById("window1").setAttribute('class', 'unfocus window');
 });
